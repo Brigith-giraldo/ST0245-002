9b2ca5503a269c1d0cd5b516b40724f585b42d85
mrsossa <kasossac@eafit.edu.co>
2020-10-28 19:50:26 -0500

    taller12

---
+++ talleres/taller12/c++/punto4.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#define D(x) cout << #x << ": " << x << endl;
+#define forn(i,n) for(int i=0; i< (int)n; i++)
+#define for1(i,n) for(int i=1; i<= (int)n; i++)
+#define all(v) v.begin(),v.end()
+
+using namespace std;
+
+typedef long long ll;
+
+void bfs(int x, vector<vector<int>> &g, vector<bool> &visited){
+    int curr,next;
+    stack<int> s;
+    s.push(x);
+    while(!s.empty()){
+        curr = s.top();
+        s.pop();
+        visited[curr] = true;
+        cout << "vertice: " << curr << endl;
+        forn(i,g[curr].size()){
+            next = g[curr][i];
+            if (!visited[next]) s.push(next);
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    cout<< setprecision(20)<< fixed;
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+    #else
+        #define endl '\n'
+    #endif
+
+    int v,e,x,y;
+    cout << "Numero de vertices y aristas?" << endl;
+    cin >> v >> e;
+    vector<vector<int>> g(v);
+    vector<bool> visited(v);
+    forn(i,e){
+        cout << "Ingrese arista" << endl;
+        cin >> x >> y;
+        g[x].push_back(y);
+    }
+    forn(i,v){
+        if (!visited[i]) bfs(i,g,visited);
+    }
+    return 0;
+}
