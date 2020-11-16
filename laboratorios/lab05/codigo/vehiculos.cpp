23172202b08465a849f5f87cb9ef4b565d8c817d
mrsossa <kasossac@eafit.edu.co>
2020-11-16 11:39:33 -0500

    memory usage

---
+++ laboratorios/lab05/codigo/vehiculos.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int g[206][206];
+int visited[206];
+int wdist[206];
+int ndist[206];
+
+void bfs(int v){
+    int auxvisited[206];
+    queue<pair<int,int>> q;
+    q.push({0,v});
+    while (!q.empty()){
+        pair<int,int> curr;
+    }
+}
+
+int main(){
+
+}
--- proyecto/codigo/MainCode.java
+++ proyecto/codigo/MainCode.java
@@ -1,16 +1,36 @@
 
 import src.*;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.util.List;
 
 public class MainCode {
     public static void main(String[] args) {
+        MemoryMXBean mbean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage memoryStart , memoryEnd;
         long start,end;
+        
+        System.gc();
         start = System.currentTimeMillis();
-        Forest f1 = new Forest(DatosEstu.leerArchivo("data/4_train_balanced_135000.csv"),100);
-        end = System.currentTimeMillis();
-        System.out.println("Forest time: "+(end-start));
+        memoryStart = mbean.getHeapMemoryUsage();
 
+        Forest f1 = new Forest(DatosEstu.leerArchivo("data/4_train_balanced_135000.csv"),100);
+        
+        System.gc();
+        end = System.currentTimeMillis();
+        memoryEnd = mbean.getHeapMemoryUsage();
+        
+        System.out.println("\n#############################");
+        System.out.println("Forest time: "+(end-start)/1000+"s");
+        System.out.println("Forest memory: "+(memoryEnd.getUsed()-memoryStart.getUsed())/1000000+"mb");
+        System.out.println("#############################\n");
+
+        System.gc();
         start = System.currentTimeMillis();
+        memoryStart = mbean.getHeapMemoryUsage();
+        
         List<String[]> test = DatosEstu.leerArchivo("data/4_test_balanced_45000.csv");
         float falsepositive = 0,falsenegative=0,truepositive=0,truenegative=0,cant=0;
         //tree.print();
@@ -31,15 +51,15 @@ public class MainCode {
                 }
             }
         }
+        System.gc();
         end = System.currentTimeMillis();
-        System.out.println("Test time: "+(end-start));
-        //System.out.println("truepositives: "+truepositive);
-        //System.out.println("truenegatives: "+truenegative);
-        //System.out.println("falsepositives: "+falsepositive);
-        //System.out.println("falsenegative: "+falsenegative);
-        //System.out.println("Correct: "+yes
-        //                +"\nIncorrect: "+not
-        //                +"\nCant: "+cant);
+        memoryEnd = mbean.getHeapMemoryUsage();
+
+        System.out.println("\n#############################");
+        System.out.println("Test time: "+(end-start)/1000+"s");
+        System.out.println("Test memory: "+(memoryEnd.getUsed()-memoryStart.getUsed())/1000000+"mb");
+        System.out.println("#############################\n");
+        
         System.out.println("Accuracy: "+( (truepositive+truenegative) / (cant))*100);
         System.out.println("Recall: "+(truepositive/(truepositive+falsenegative))*100);
         System.out.println("Precision: "+(truepositive/(truepositive+falsepositive))*100+"%");
